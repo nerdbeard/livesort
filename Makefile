422c425c7612ca68fd3d4f5a2b7c263afced2593
Mike Pelletier <mike@mkp.ca>
2021-04-22 08:48:41 -0400

    Sort and present line-based data as it is read
    A simple program written in several languages for the purpose of
    examining these languages.

---
+++ Makefile
@@ -0,0 +1,5 @@
+test:
+	python3.8 -m unittest
+
+clean:
+	rm livesort_hssp/test_livesort.py livesort_hssp/livesort.py
---
+++ livesort_hssp/__init__.py
@@ -0,0 +1,5 @@
+from hissp.reader import transpile
+transpile(__package__, "livesort", "test_livesort")
+del transpile
+
+from livesort_hssp.livesort import livesort, render
