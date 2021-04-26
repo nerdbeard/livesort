782fa68221d4672a65dd64f20fa33eb2ab9d40b8
Mike Pelletier <mike@mkp.ca>
2021-04-26 11:53:28 -0400

    Basic function kinda works

--- livesort_hssp/__init__.py
+++ livesort_hssp/__init__.py
@@ -2,4 +2,4 @@ from hissp.reader import transpile
 transpile(__package__, "livesort", "test_livesort")
 del transpile
 
-from livesort_hssp.livesort import livesort, render
+from livesort_hssp.livesort import Livesort
